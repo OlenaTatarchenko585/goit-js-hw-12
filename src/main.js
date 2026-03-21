871d09651171575e000bcea7763fe8d9b8569a7f
Olena Tatarchenko <olenatatarch585@gmail.com>
2026-03-21 12:08:42 +0200

    My js-HW-12

---
+++ src/main.js
@@ -0,0 +1,72 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader, showLoadMoreButton, hideLoadMoreButton } from './js/render-functions.js';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+let query = '';
+let page = 1;
+let totalPages = 0;
+
+const form = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  query = event.currentTarget.elements.query.value.trim();
+  
+  if (!query) return;
+
+  page = 1;
+  clearGallery();
+  hideLoadMoreButton();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+    hideLoader();
+
+    if (data.hits.length === 0) {
+      iziToast.error({ message: 'Sorry, there are no images matching your search query.' });
+      return;
+    }
+
+    createGallery(data.hits);
+    totalPages = Math.ceil(data.totalHits / 15);
+
+    if (page < totalPages) {
+      showLoadMoreButton();
+    }
+  } catch (error) {
+    hideLoader();
+    iziToast.error({ message: 'Something went wrong!' });
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  hideLoadMoreButton();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+    hideLoader();
+    createGallery(data.hits);
+    
+    // Плавний скрол
+    const card = document.querySelector('.gallery-item');
+    if (card) {
+      const { height } = card.getBoundingClientRect();
+      window.scrollBy({ top: height * 2, behavior: 'smooth' });
+    }
+
+    if (page >= totalPages) {
+      hideLoadMoreButton();
+      iziToast.info({ message: "We're sorry, but you've reached the end of search results." });
+    } else {
+      showLoadMoreButton();
+    }
+  } catch (error) {
+    hideLoader();
+    iziToast.error({ message: 'Failed to load more images.' });
+  }
+});
